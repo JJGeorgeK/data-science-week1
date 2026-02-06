6c6d18b00217e602096dd36e1cee1dfb98a5a19f
JJGeorgeK <joelgeorgek2005@gmail.com>
2026-02-06 14:12:55 +0000

    week 2: fixing 2 issues

--- scripts/initial_exploration.R
+++ scripts/initial_exploration.R
@@ -1,6 +1,6 @@
 # Week 1: Initial Data Exploration ====
-# Author: [Your Name]
-# Date: [Today's Date]
+# Author: [Joel]
+# Date: [06-02-2026]
 
 # Load packages ====
 library(tidyverse)
@@ -40,3 +40,58 @@ mosquito_egg_raw |>
 #   body mass is negative
 # - Any obvious problems?
 #   some values are missing
+
+
+
+# FIX 1: [fixing negative values of body mass] ====
+
+# Show the problem:
+# [Code to demonstrate issue exists]
+mosquito_egg_raw |> 
+  filter(body_mass_mg <= 0)
+
+
+# Fix it:
+mosquito_egg_data_step1 <- mosquito_egg_raw |>
+  # YOUR CODE HERE
+  # Check for zero or negative values where zero doesn't make biological sense
+  mosquito_egg_data_step1 <- mosquito_egg_raw |>
+  mutate(
+    body_mass_mg = if_else(body_mass_mg <= 0, NA_real_, body_mass_mg)
+  )
+
+  
+  # Verify it worked:
+  # [Code to check change happened]
+  mosquito_egg_data_step1 |>
+    filter(body_mass_mg <= 0)
+  
+  # What changed and why it matters:
+  # [2-3 sentences explaining consequences]
+  #the negative values of the body_mass changed, this is important as body mass 
+  #can not be less than or equal to 0
+  
+  # FIX 2: [there are inconsistent capital letters throughout the data]  ====
+
+# Show the problem:
+# [Code]
+  mosquito_egg_raw |>
+    count(treatment)
+
+# Fix it:
+mosquito_egg_data_step2 <- mosquito_egg_data_step1 |>
+  # YOUR CODE
+  mosquito_egg_data_step2 <- mosquito_egg_data_step1 |>
+  mutate(
+    treatment = str_to_lower(treatment)
+  )
+  
+  # Verify it worked:
+  # [Code]
+mosquito_egg_data_step2 |>
+  count(treatment)
+  
+  # What changed and why it matters:
+  # [2-3 sentences]
+  # the treatment shows up to be lower case, and instead of a tibble of 12x2, 
+#it has gone to 4x2 of meaningful treatment names.
